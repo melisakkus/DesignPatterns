59814bab3f8fbed55f098a422c413efba8a0e511
Melisa Akkuş <melisa.akkus01@gmail.com>
2024-08-28 22:04:31 +0300

    Adapter and Composite Desingn Patterns Added

---
+++ Adapter/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager1 = new ProductManager(new MlsLogger());
+            productManager1.Save();
+
+            ProductManager productManager2 = new ProductManager(new Log4NetAdapter());
+            productManager2.Save();
+        }
+    }
+
+    class ProductManager
+    {
+        private ILogger _logger;
+        public ProductManager(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Save()
+        {
+            _logger.Log("User Data");
+            Console.WriteLine("Saved");
+        }
+    }
+
+    interface ILogger
+    {
+        void Log(string message);
+    }
+
+    class MlsLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine("Logged,{0}",message);
+        }
+    }
+
+    //yeni bir loglama frameworkü kullanmak istersek? NuGetten aldık, classı değiştiremiyoruz
+    class Log4Net
+    {
+        public void LogMessage(string message)
+        {
+            Console.WriteLine("Logged,{0}", message);
+        }
+    }
+
+    //productManager da log4Net'i productManager içerisine dokunmadan
+    //(SOLID-O : yeni bir özellik eklediğinde mevcut sistemin değişikliğe uğramaması) nasıl çağırabilirim?
+
+    class Log4NetAdapter : ILogger
+    {
+        public void Log(string message)
+        {
+            Log4Net log4Net = new Log4Net();
+            log4Net.LogMessage(message);
+        }
+    }
+}
+
+//özellikle farklı sistemlerin kendi sistemlerimize entegre edilmesi durumunda
+//kendi sistemimiz bozulmadan farklı sistemin bizim sistemimizmiş gibi davranmasını sağlamak 
+
+//bir servisi referans ettiğimizde direkt olarak metot içerisinde kullanırsak ona bağımlı kalırız
+//bu bağımlılığın önüne geçmek istiyoruz
+
+//ILogger cinsini istemiştim ProductManagerda. Yeni sistemi bir adapter ile ILogger'a adapte edebilirim.
---
+++ Composite/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee melisa = new Employee { Name = "Melisa Akkuş" };
+            Employee ahmet = new Employee { Name = "Ahmet Akkuş" };
+            Employee sena = new Employee { Name = "Sena Akkuş" };
+            Employee pilotlar = new Employee { Name = "THY pilotları" };
+            Employee doktorlar = new Employee { Name = "Ankara Hastanesi Çalışanları" };
+
+            //ağaç yapısı
+            melisa.AddSubordinate(ahmet);
+            melisa.AddSubordinate(sena);
+            ahmet.AddSubordinate(pilotlar);
+            sena.AddSubordinate(doktorlar);
+
+            Console.WriteLine("Yönetici: " + melisa.Name);
+            foreach (Employee manager in melisa)
+            {
+                Console.WriteLine("  Müdür: " + manager.Name);
+                foreach(IPerson employee in manager)
+                {
+                    Console.WriteLine("    Çalışan: " + employee.Name);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+    
+    interface IPerson
+    {
+        string Name { get; set; }
+    }
+
+    class Employee : IPerson, IEnumerable<IPerson>
+    {
+        List<IPerson> _subordinates = new List<IPerson>();
+
+        public void AddSubordinate(IPerson person) //hiyerarşiye birisini eklemek
+        {
+            _subordinates.Add(person);
+        }
+
+        public void RemoveSubordinate(IPerson person) //hiyerarşiyden birisini çıkarmak
+        {
+            _subordinates.Remove(person);
+        }
+
+        public IPerson GetSubordinate(int index)//hiyerarşideki bir nesneye ulaşmak
+        {
+            return _subordinates[index];
+        }
+
+        public string Name { get; set; }
+
+        public IEnumerator<IPerson> GetEnumerator()
+        {
+            foreach (var subordinate in _subordinates)
+            {
+                yield return subordinate;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+}
+
+//nesneler arası hiyerarşi ve bu hiyerarşik nesnelere istediğimiz zaman ulaşabilmek
+//bir kurumdaki roller ve bu rollerin ağaç yapısı gibi
+//nesnelere ulaşabilmek için (hiyerarşik yapı) Enumerable bir yapı ile implemente ettik
+//foreach ile gezmemizi sağlaycak IEnumerable özelliği katmak ve liste vasıtasıyla altnesnelere ekleme çıkarmalar yapmak
+
+//public IEnumerator<IPerson> GetEnumerator()
+//{
+//    throw new NotImplementedException();
+//}
+
+//IEnumerator IEnumerable.GetEnumerator()
+//{
+//    throw new NotImplementedException();
+//}
+
+//IPerson kullanarak tedarikçi gibi farklı sınıflarıda dahil edip AddSubordinate ile hiyerarşiye ekleyebilirim
