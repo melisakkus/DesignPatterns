78578eb8c9060613296b9864f0b5e25f7c6ad18a
Melisa Akkuş <melisa.akkus01@gmail.com>
2024-08-31 22:54:30 +0300

    Decorator and Proxy are added.

---
+++ Decorator/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Instrumentation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var personelCar = new PersonelCar { Make = "BMW", Model = "3.2", HirePrice= 1000};
+            SpecialOffer specialOffer = new SpecialOffer(personelCar);
+            specialOffer.DiscountPercentage = 20;
+
+            Console.WriteLine("Concrete : " + personelCar.HirePrice);
+            Console.WriteLine("Special offer : " + specialOffer.HirePrice);
+            Console.ReadLine();
+        }
+    }
+
+    abstract class CarBase
+    {
+        public abstract string Make { get; set; }
+        public abstract string Model { get; set; }
+        public abstract decimal HirePrice { get; set; }
+    }//farklı durumlarda bu asbtractları override edeceğiz
+
+    class PersonelCar : CarBase
+    {
+        public override string Make { get; set; }
+        public override string Model { get; set; }
+        public override decimal HirePrice { get; set; }
+    }
+
+    class CommercialCar : CarBase
+    {
+        public override string Make { get; set; }
+        public override string Model { get; set; }
+        public override decimal HirePrice { get; set; }
+        }
+
+    abstract class CarDecoratorBase : CarBase
+    {
+        private CarBase _carBase;
+        protected CarDecoratorBase(CarBase carBase)
+        {
+            _carBase = carBase; 
+        }
+    }
+
+    class SpecialOffer : CarDecoratorBase
+    {
+        public int DiscountPercentage {  get; set; }
+        private readonly CarBase _carBase;
+        public SpecialOffer(CarBase carBase) : base(carBase)
+        {
+            _carBase = carBase;
+        }
+
+        public override string Make { get; set; }
+        public override string Model { get; set; }
+        public override decimal HirePrice
+        {
+            get
+            { return _carBase.HirePrice - _carBase.HirePrice * DiscountPercentage / 100; }
+            set { }
+        }
+    }
+}
+
+//elimizdeki temel nesneyi farklı koşullarda farklı anlamlarla kullanmak 
+//bir araç kiralama firmasında bazı dönemlerde bazı kullanıcılar için farklı indirimler gibi 
---
+++ Proxy/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //CreditManager manager = new CreditManager();
+            CreditBase manager = new CreditManagerProxy();
+            Console.WriteLine(manager.Calculate());
+            Console.WriteLine(manager.Calculate());
+            Console.WriteLine(manager.Calculate());
+
+            Console.ReadLine();
+        }
+    }
+
+    abstract class CreditBase //temel operasyon sınıfımız
+    {
+        public abstract int Calculate();
+    }
+
+    class CreditManager : CreditBase
+    {
+        public override int Calculate()
+        {//kredi hesaplamasında iflerle halletmek yerine concrete oluşturup onunla işlemi yapmak
+
+            int result = 1;
+            for(int i = 1 ; i < 5; i++)
+            {
+                result *= i ;
+                Thread.Sleep(1000);
+            }
+            return result;
+        }
+    }
+
+    class CreditManagerProxy : CreditBase
+    {
+        private CreditManager _creditManager;
+        int _cachedValue;
+        public override int Calculate()
+        {
+            if(_creditManager == null)
+            {
+                _creditManager = new CreditManager();
+                _cachedValue = _creditManager.Calculate();
+            }
+            return _cachedValue;
+        }
+    }
+}
+
+// Bir sınıfın çağırdığı bir işlem var, ilk kez çağrıldığında onu oluştur, tekrar çağrıldığında daha önce çağrıldığındakini kullanma süreci üzerine
+// basit bir cacheleme gibi
+// bir kaynak veya hesaplama sonucu sabit bir işlem gibiyse kullanılabilir
+// Thread.Sleep(1000); 1000 milisaniye(1 saniye) süreyle beklemesini sağlar
